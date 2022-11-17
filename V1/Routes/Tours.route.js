a85ce6f40b2fc808db71610619a155ab2f287744
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-17 21:09:37 +0600

    add cheapest tour

--- V1/Routes/Tours.route.js
+++ V1/Routes/Tours.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const { getTours, postTours, getTourById, getTrendingTour } = require('../Controllers/Tours.controller');
+const { getTours, postTours, getTourById, getTrendingTour, getCheapestTour } = require('../Controllers/Tours.controller');
 
 const router = express.Router();
 
 router.route('/')
     .get(getTours)
     .post(postTours)
-    
+
 router.route('/trending').get(getTrendingTour)
+router.route('/cheapest').get(getCheapestTour)
 
 router.route('/:id').get(getTourById)
 
