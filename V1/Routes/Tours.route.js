852eca06ad98d6f8dd15d68841be5fbf12850023
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-17 21:15:48 +0600

    finalize assignment

--- V1/Routes/Tours.route.js
+++ V1/Routes/Tours.route.js
@@ -3,12 +3,37 @@ const { getTours, postTours, getTourById, getTrendingTour, getCheapestTour, upda
 
 const router = express.Router();
 
+
+/**
+ * @api {get} Get all the tour details here
+ * @require {admin}
+ */
+
+/**
+ * @api {post} Post tour here
+ * required field {name, description, price, tickets (in-stock, out-of-stock)}
+ */
 router.route('/')
     .get(getTours)
     .post(postTours)
 
+
+/**
+ * @api {Get} Trending tour 
+ */
+/**
+ * @api {Get} Cheapest tour 
+ */
 router.route('/trending').get(getTrendingTour)
 router.route('/cheapest').get(getCheapestTour)
+
+
+/**
+ * @api {Get tour} get tour by using id
+ */
+/**
+ * @api {Update tours} update tour by using id
+ */
 
 router.route('/:id')
     .get(getTourById)
