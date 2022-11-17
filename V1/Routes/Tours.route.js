fc0175ac98f3a147a547ea10fa93e320d857b02a
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-17 21:08:32 +0600

    tending tours

--- V1/Routes/Tours.route.js
+++ V1/Routes/Tours.route.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { getTours, postTours, getTourById } = require('../Controllers/Tours.controller');
+const { getTours, postTours, getTourById, getTrendingTour } = require('../Controllers/Tours.controller');
 
 const router = express.Router();
 
 router.route('/')
     .get(getTours)
     .post(postTours)
+    
+router.route('/trending').get(getTrendingTour)
 
 router.route('/:id').get(getTourById)
+
 
 
 
